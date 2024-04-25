3bc2fd3e14a73409e318f3f574825ac3a2de32b9
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-04-25 11:42:53 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/README.md
+++ 프로그래머스/0/120817. 배열의 평균값/README.md
@@ -1,6 +1,6 @@
 # [level 0] 배열의 평균값 - 120817 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120817) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120817?language=javascript) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 11:38:21
+2024년 04월 25일 11:42:51
 
 ### 문제 설명
 
--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,10 +1,14 @@
 function solution(numbers) {
     var answer = 0;
-    let sum = 0;
+//     let sum = 0;
     
-    numbers.forEach(n => {
-        sum += n;
-    });
+//     numbers.forEach(n => {
+//         sum += n;
+//     });
+//     answer = sum / numbers.length;
+    let sum = numbers.reduce((n1,n2)=>n1+n2, 0);
+    
     answer = sum / numbers.length;
+    
     return answer;
 }
