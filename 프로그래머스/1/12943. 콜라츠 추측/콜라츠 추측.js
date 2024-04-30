86e3d3487507a6521749d2a4865fb69587fe3862
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-04-30 09:26:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -2,11 +2,12 @@ function solution(num) {
     var answer = 0;
     
     while(num !== 1){
-        //num이 홀수
-        if(num % 2)
-            num = num*3 +1;
-        //num이 짝수
-        else num /= 2;
+        // //num이 홀수
+        // if(num % 2)
+        //     num = num*3 +1;
+        // //num이 짝수
+        // else num /= 2;
+        num = (num%2) ? num*3+1 : num/2;
         
         if(++answer > 500){
             answer = -1;
