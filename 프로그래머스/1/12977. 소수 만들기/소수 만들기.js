ae80f6f859ab27dc64f416374abdffd85cbb0094
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-05-14 09:38:41 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,24 @@
+function solution(nums) {
+    const sum = [];
+    let count = 0;
+    
+    for(let i=0; i<nums.length-2; i++){
+        for(let j=i+1; j<nums.length-1; j++){
+            for(let k=j+1; k<nums.length; k++){
+                sum.push(nums[i]+nums[j]+nums[k]);
+            }
+        }
+    }
+    
+    for(let i=0; i<sum.length; i++){
+        if(isPrime(sum[i])) count++;
+    }
+    
+    return count;
+}
+
+function isPrime(n){
+    for(let i=2; i<=Math.sqrt(n); i++)
+        if(!(n%i)) return false;
+    return true;
+}
