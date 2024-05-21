9b41cd5e8d5f694070f851c30d0afa844aa85da9
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-05-21 12:18:41 +0900

    [level 1] Title: 체육복, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,33 +1,54 @@
 function solution(n, lost, reserve) {
     var answer = 0;
     
-    lost.sort((a,b)=>a-b);
-    reserve.sort((a,b)=>a-b);
+//     lost.sort((a,b)=>a-b);
+//     reserve.sort((a,b)=>a-b);
     
-    for(let i=0; i<lost.length; i++){
-        // const tmp = reserve.indexOf(lost[i]);
-        // if(tmp !== -1) {reserve.splice(tmp,1); lost.splice(i,1); i--;}
-        for(let j=0; j<reserve.length; j++){
-            if(lost[i] === reserve[j]){
-                lost.splice(i,1);
-                reserve.splice(j,1);
-                i--;
-                break;
-            }
-        }
-    }
+//     for(let i=0; i<lost.length; i++){
+//         // const tmp = reserve.indexOf(lost[i]);
+//         // if(tmp !== -1) {reserve.splice(tmp,1); lost.splice(i,1); i--;}
+//         for(let j=0; j<reserve.length; j++){
+//             if(lost[i] === reserve[j]){
+//                 lost.splice(i,1);
+//                 reserve.splice(j,1);
+//                 i--;
+//                 break;
+//             }
+//         }
+//     }
     
-    for(const i of lost){
-        for(let j = 0; j<reserve.length; j++){
-            if(reserve[j] === i-1 || reserve[j] === i+1){
+//     for(const i of lost){
+//         for(let j = 0; j<reserve.length; j++){
+//             if(reserve[j] === i-1 || reserve[j] === i+1){
+//                 answer++;
+//                 reserve.splice(j,1);
+//                 break;
+//             }
+//         }
+//     }
+    
+//     answer = n - lost.length + answer;
+    
+    const suit = new Array(n);
+    suit.fill(1);
+    for(const i of lost)
+        suit[i-1]--;
+    for(const i of reserve)
+        suit[i-1]++;
+    
+    for(let i=0; i<n; i++){
+        if(!suit[i]){
+            if(i && suit[i-1] === 2){
+                suit[i-1] = 1;
+                suit[i] = 1;
                 answer++;
-                reserve.splice(j,1);
-                break;
+            } else if(i !== n-1 && suit[i+1] === 2){
+                suit[i+1] = 1;
+                suit[i] = 1;
+                answer++;
             }
-        }
+        } else if(suit[i] >= 1) answer++;
     }
-    
-    answer = n - lost.length + answer;
     
     return answer;
 }
