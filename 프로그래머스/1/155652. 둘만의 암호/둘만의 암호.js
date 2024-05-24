ce1cc56a02cd80ed0515235dd5c1b92947c88d7d
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-05-24 09:34:28 +0900

    [level 1] Title: 둘만의 암호, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,22 @@
+function solution(s, skip, index) {
+    var answer = '';
+
+    for(let i=0; i<s.length; i++){
+        let j=0;
+        let tmp = s.charCodeAt(i);
+        
+        while(j<index){
+            tmp++;
+            if(tmp > 'z'.charCodeAt(0)){
+                tmp -= 26;
+            }
+            if(!skip.includes(String.fromCharCode(tmp))){
+                j++;
+            } 
+        }
+        
+        answer += String.fromCharCode(tmp);
+    }
+    
+    return answer;
+}
