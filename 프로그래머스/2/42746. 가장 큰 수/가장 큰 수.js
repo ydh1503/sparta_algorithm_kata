45d7fd9aaaa0101afd9731b2d99615227f0acde8
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-07-10 11:23:28 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,33 +2,40 @@ function solution(numbers) {
     var answer = '';
     
     numbers = numbers.map((e)=>e.toString());
-    numbers.sort((a,b)=>{
-        if(a.length > b.length) {
-            return compare(b,a);
-        } else 
-            return compare(a,b) * -1;
-    });
+    // numbers.sort((a,b)=>{
+    //     if(a.length > b.length) {
+    //         return compare(b,a);
+    //     } else 
+    //         return compare(a,b) * -1;
+    // });
+    numbers.sort(compare1);
     
     answer = numbers.join('');
-    if(answer.split('').every(e=>e==='0'))
+    // if(answer.split('').every(e=>e==='0'))
+    //     answer = '0';
+    if(answer[0] === '0')
         answer = '0';
     
     return answer;
 }
 
-function compare(short, long) {
-    for(let i=0; i<long.length; i++){
-        const shortIndex = i < short.length ? i : 0;
-        if(short[shortIndex]>long[i])
-            return 1;
-        else if(short[shortIndex]<long[i])
-            return -1;
-    }
-    for(let i=1; i<short.length; i++){
-        if(short[i] > short[0])
-            return 1;
-        else if(short[i] < short[0])
-            return -1;
-    }
-    return 0;
+// function compare(short, long) {
+//     for(let i=0; i<long.length; i++){
+//         const shortIndex = i < short.length ? i : 0;
+//         if(short[shortIndex]>long[i])
+//             return 1;
+//         else if(short[shortIndex]<long[i])
+//             return -1;
+//     }
+//     for(let i=1; i<short.length; i++){
+//         if(short[i] > short[0])
+//             return 1;
+//         else if(short[i] < short[0])
+//             return -1;
+//     }
+//     return 0;
+// }
+
+function compare1(a,b){
+    return a+b > b+a ? -1 : 1;
 }
