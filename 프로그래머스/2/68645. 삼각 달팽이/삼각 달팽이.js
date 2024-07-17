f5715e8a824ac261c779e231a78690000e67e47e
ydh1503 <84895591+ydh1503@users.noreply.github.com>
2024-07-17 10:00:27 +0900

    [level 2] Title: 삼각 달팽이, Time: 3148.07 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,26 @@
+function solution(n) {
+    var answer = [];
+    const snail = [];
+    let i=1;
+    let x=-1, y=0;
+    let routine = 0;
+    
+    while(n>0){
+        for(let j=0; j<n; j++){
+            switch(routine){
+                case 0: x++; break;
+                case 1: y++; break;
+                case 2: x--; y--; break;
+            }
+            snail[x] ??= [];
+            snail[x][y] = i++;
+        }
+        routine++;
+        routine %= 3;
+        n--;
+    }
+    console.log(snail);
+    answer = snail.reduce((acc,cur)=>[...acc,...cur],[]);
+    
+    return answer;
+}
